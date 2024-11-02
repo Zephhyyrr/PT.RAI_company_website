347a755c4234b6eff8a7a602c64133f5955133d0
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-11-02 19:40:19 +0700

    auu ahh cape

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -7,7 +7,18 @@
                     <p class="who-we-are">Who We Are</p>
                     <h3>Rumpun Alam Indonesia</h3>
                     <p class="fst">
-                        PT. Rumpun Alam Indonesia is a business that was founded in 2021 starting in the field of manufacturing, processing (production) of local spices that dominate the results of the natural harvest of West Sumatra, Indonesia, which is monitored and supervised  directly by the provincial trade and agriculture office of West Sumatra West Sumatra for feasibility testing and food security. Existence The processing began to develop in 2022. Where is the place this place is a unity from the start of the planting process to processing of the entire harvest is manual so that requires a relatively large number of workers even though not all workers do sorting at the production site. labor does the sorting at the production site. with a lot of market demand and a lot of interest and demand for export imports. market demand as well as the many interests and requests for export and import from various countries regarding this matter in 2023 we officially investigated PT or a company that is registered and incorporated with the the official legality of the Indonesian nationality. 
+                        PT. Rumpun Alam Indonesia is a business that was founded in 2021 starting in the field of
+                        manufacturing, processing (production) of local spices that dominate the results of the natural
+                        harvest of West Sumatra, Indonesia, which is monitored and supervised directly by the provincial
+                        trade and agriculture office of West Sumatra West Sumatra for feasibility testing and food
+                        security. Existence The processing began to develop in 2022. Where is the place this place is a
+                        unity from the start of the planting process to processing of the entire harvest is manual so
+                        that requires a relatively large number of workers even though not all workers do sorting at the
+                        production site. labor does the sorting at the production site. with a lot of market demand and
+                        a lot of interest and demand for export imports. market demand as well as the many interests and
+                        requests for export and import from various countries regarding this matter in 2023 we
+                        officially investigated PT or a company that is registered and incorporated with the the
+                        official legality of the Indonesian nationality.
                     </p>
                     <ul>
                         <p class="fst fs-6 fw-bold">
@@ -15,11 +26,13 @@
                         </p>
                         <li>
                             <i class="bi bi-check-circle"></i>
-                            <span>Introducing the natural wealth of West Sumatra - Indonesia to the wider community both locally and internationally</span>
+                            <span>Introducing the natural wealth of West Sumatra - Indonesia to the wider community both
+                                locally and internationally</span>
                         </li>
                         <li>
                             <i class="bi bi-check-circle"></i>
-                            <span>Preserving Indonesia's heritage and culture as an internationally recognized spice producer </span>
+                            <span>Preserving Indonesia's heritage and culture as an internationally recognized spice
+                                producer </span>
                         </li>
                         <li>
                             <i class="bi bi-check-circle"></i>
@@ -35,20 +48,20 @@
                 <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
                     <div class="row gy-4">
                         <div class="col-lg-6">
-                            <img src="assets/img/about-company-1.jpg" class="img-fluid" alt="" />
+                            <img src="assets/img/dummy/kapulaga/kapulaga1.jpg" class="img-fluid" alt="" />
                         </div>
                         <div class="col-lg-6">
-                            <div class="row gy-4">
-                                <div class="col-lg-12">
-                                    <img src="assets/img/about-company-2.jpg" class="img-fluid" alt="" />
-                                </div>
-                                <div class="col-lg-12">
-                                    <img src="assets/img/about-company-3.jpg" class="img-fluid" alt="" />
-                                </div>
-                            </div>
+                            <img src="/assets/img/dummy/kayumanis/kayumanis1.jpg" class="img-fluid" alt="" />
+                        </div>
+                        <div class="col-lg-6">
+                            <img src="/assets/img/dummy/kayumanis/kayumanis1.jpg" class="img-fluid" alt="" />
+                        </div>
+                        <div class="col-lg-6">
+                            <img src="/assets/img/dummy/kayumanis/kayumanis1.jpg" class="img-fluid" alt="" />
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </section>

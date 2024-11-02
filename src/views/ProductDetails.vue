08b3b9f914fd3f392522eca68fa81ecb10323940
Baghaztra <baghaztra21@gmail.com>
2024-11-02 23:40:19 +0700

    Update product

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -21,92 +21,112 @@
     <!-- ini tampilan sementara -->
 
     <!-- Features Details Section -->
-    <section id="features-details" class="features-details section light-background">
-
+    <section
+      v-for="(featureData, key, index) in featureSections"
+      :key="key"
+      :class="['features-details', 'section', index % 2 === 0 ? 'light-background' : '']"
+    >
       <div class="container">
-
         <div class="row gy-4 justify-content-between features-item">
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <img :src="product.images[1]" class="img-fluid" alt="">
-          </div>
-
-          <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+          <div
+            :class="['col-lg-5', 'd-flex', 'align-items-center', index % 2 === 0 ? 'order-2 order-lg-1' : 'order-1 order-lg-2']"
+            data-aos="fade-up"
+            :data-aos-delay="index * 100 + 100"
+          >
             <div class="content">
-              <h3>Introducing {{ product.title }}</h3>
-              <ul>
-                <li v-for="(value, key) in product.intro" :key="key">
-                  <strong>{{ key }} : </strong> {{ value }}
+              <h3>{{ formatTitle(key) }}</h3>
+              <!-- Kondisi jika hanya satu item, tampilkan langsung -->
+              <p v-if="typeof featureData === 'string'">{{ featureData }}</p>
+              <!-- Jika lebih dari satu item, tampilkan dalam list -->
+              <ul v-else>
+                <li v-for="(value, subKey) in featureData" :key="subKey">
+                  <strong>{{ formatTitle(subKey) }}:</strong> {{ value }}
                 </li>
               </ul>
             </div>
           </div>
-
+          <div
+            :class="['col-lg-6', index % 2 === 0 ? 'order-1 order-lg-2' : 'order-2 order-lg-1']"
+            data-aos="fade-up"
+            :data-aos-delay="index * 100 + 200"
+          >
+            <img :src="product.images[index % product.images.length]" class="img-fluid img-feature" alt="" />
+          </div>
         </div>
-
-        <div class="row gy-4 justify-content-between features-item">
-
-          <div class="col-lg-5 d-flex align-items-center order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="content">
-              <h3>Benefit : {{ product.title }}</h3>
-              <ul>
-                <li v-for="(value, key) in product.benefit" :key="key">
-                  <strong>{{ key }} : </strong>{{ value }}
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
-            <img :src="product.images[0]" class="img-fluid" alt=""/>
-          </div>
-
-        </div><!-- Features Item -->
-
       </div>
-
+    </section>
+    <section class="features-details section light-background">
+      <div class="container">
+        <div class="row gy-4 justify-content-between features-item">
+          <div class="content">
+            <!-- <h3>{{ formatTitle(key) }}</h3> -->
+            <p>{{ product.closing }}</p>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
-  import products_en from "../data/products-en.json";
-  import products_cn from "../data/products-cn.json";
-  import products_id from "../data/products-id.json";
+import products_en from "../data/products-en.json";
+import products_cn from "../data/products-cn.json";
+import products_id from "../data/products-id.json";
 
-  export default {
-    name: "ProductDetail",
-    data() {
-      return {
-        product: {},
-      };
+export default {
+  name: "ProductDetail",
+  data() {
+    return {
+      product: {}
+    };
+  },
+  created() {
+    this.loadProductEn();
+  },
+  computed: {
+    featureSections() {
+      // Filter featureSections agar hanya mencakup data tanpa title dan closing
+      return Object.keys(this.product).reduce((sections, key) => {
+        if (key !== 'id' && key !== 'images' && key !== 'title' && key !== 'closing') {
+          sections[key] = this.product[key];
+        }
+        return sections;
+      }, {});
+    }
+  },
+  methods: {
+    loadProductEn() {
+      const productId = this.$route.params.id;
+      this.product = products_en.find((p) => p.id === productId);
     },
-    created() {
+    loadProductCn() {
+      const productId = this.$route.params.id;
+      this.product = products_cn.find((p) => p.id === productId);
+    },
+    loadProductId() {
+      const productId = this.$route.params.id;
+      this.product = products_id.find((p) => p.id === productId);
+    },
+    formatTitle(key) {
+      return key
+        .replace(/_/g, " ")
+        .replace(/\b\w/g, (char) => char.toUpperCase());
+    },
+    isMultiItem(featureData) {
+      return typeof featureData === 'object' && Object.keys(featureData).length > 1;
+    }
+  }, 
+  // kalau parameter berubah, ulang laod lagi 
+  watch: {
+    '$route.params.id': function() {
       this.loadProductEn();
     },
-    watch: {
-      "$route.params.id": "loadProduct",
-    },
-    methods: {
-      loadProductEn() {
-        const productId = this.$route.params.id;
-        this.product = products_en.find((p) => p.id === productId);
-      },
-      loadProductCn() {
-        const productId = this.$route.params.id;
-        this.product = products_cn.find((p) => p.id === productId);
-        console.log("China Product:", this.product);
-      },
-      loadProductId() {
-        const productId = this.$route.params.id;
-        this.product = products_id.find((p) => p.id === productId);
-      },
-    },
-
-  };
+  },
+};
 </script>
-
 <style scoped>
-  /* Pastikan scoped untuk mengisolasi CSS ke komponen ini */
+  .img-feature{
+    width: 100%;
+    height: auto;
+  }
 </style>

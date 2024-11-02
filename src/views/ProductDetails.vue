891fbd5f1a8f1371cb185c56fa1e4b543f47df82
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-11-02 18:46:52 +0700

    push

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -2,21 +2,21 @@
   <main class="main">
 
     <section id="home" class="hero section">
-        <div class="hero-bg">
-            <img src="/assets/img/rempah_background.jpg" alt="">
+      <div class="hero-bg">
+        <img src="/assets/img/rempah_background.jpg" alt="">
+      </div>
+      <div class="container text-center">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <h1 data-aos="fade-up">Our Product <span>{{ product.title }}</span></h1>
+          <p data-aos="fade-up" data-aos-delay="100">Perusahaan Exportir Rempah-Rempah dari Indonesia<br></p>
         </div>
-        <div class="container text-center">
-            <div class="d-flex flex-column justify-content-center align-items-center">
-                <h1 data-aos="fade-up">Our Product <span>{{ product.title }}</span></h1>
-                <p data-aos="fade-up" data-aos-delay="100">Perusahaan Exportir Rempah-Rempah dari Indonesia<br></p>
-                </div>
-                <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/qMlgIQxdHO0?si=kq3DyP8VyWk8w5i-"
+        <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/qMlgIQxdHO0?si=kq3DyP8VyWk8w5i-"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
-                <!-- <img src="assets/img/hero-services-img.webp" class="img-fluid hero-img" alt="" data-aos="zoom-out"
+        <!-- <img src="assets/img/hero-services-img.webp" class="img-fluid hero-img" alt="" data-aos="zoom-out"
                     data-aos-delay="300"> -->
-            </div>
+      </div>
     </section>
     <!-- ini tampilan sementara -->
 
@@ -33,36 +33,29 @@
 
           <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
             <div class="content">
-              <h3>Corporis temporibus maiores provident</h3>
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
-              </p>
-              <a href="#" class="btn more-btn">Learn More</a>
+              <h3>Introducing {{ product.title }}</h3>
+              <ul>
+                <li v-for="(value, key) in product.intro" :key="key">
+                  <strong>{{ key }} : </strong>&nbsp; {{ value }}
+                </li>
+              </ul>
             </div>
           </div>
 
-        </div><!-- Features Item -->
+        </div>
 
         <div class="row gy-4 justify-content-between features-item">
 
           <div class="col-lg-5 d-flex align-items-center order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
 
             <div class="content">
-              <h3>Neque ipsum omnis sapiente quod quia dicta</h3>
-              <p>
-                Quidem qui dolore incidunt aut. In assumenda harum id iusto lorena plasico mares
-              </p>
+              <h3>Benefit : {{ product.title }}</h3>
               <ul>
-                <li><i class="bi bi-easel flex-shrink-0"></i> Et corporis ea eveniet ducimus.</li>
-                <li><i class="bi bi-patch-check flex-shrink-0"></i> Exercitationem dolorem sapiente.</li>
-                <li><i class="bi bi-brightness-high flex-shrink-0"></i> Veniam quia modi magnam.</li>
+                <li v-for="(value, key) in product.benefit" :key="key">
+                  <strong>{{ key }} : </strong>&nbsp;{{ value }}
+                </li>
               </ul>
-              <p></p>
-              <a href="#" class="btn more-btn">Learn More</a>
             </div>
-
           </div>
 
           <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
@@ -73,22 +66,7 @@
 
       </div>
 
-    </section><!-- /Features Details Section -->
-    <div class="container">
-      <h1>{{ product.title }}</h1>
-      <h3>Pengenalan {{ product.title }}</h3>
-      <ul>
-        <li v-for="(value, key) in product.intro" :key="key">
-          <strong>{{ key }}:</strong> {{ value }}
-        </li>
-      </ul>
-      <h1>Manfaat {{ product.title }}</h1>
-      <ul>
-        <li v-for="(value, key) in product.benefit" :key="key">
-          <strong>{{ key }}:</strong> {{ value }}
-        </li>
-      </ul>
-    </div>
+    </section>
   </main>
 </template>
 
@@ -125,7 +103,7 @@
         this.product = products_id.find((p) => p.id === productId);
       },
     },
-    
+
   };
 </script>
 

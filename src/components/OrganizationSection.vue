3d6295769f64ed307ccb93e1a657eb65be204deb
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-11-05 20:38:38 +0700

    edit data

--- src/components/OrganizationSection.vue
+++ src/components/OrganizationSection.vue
@@ -18,7 +18,7 @@
                 <h3>{{ organization.name }}</h3>
                 <h4>{{ organization.position }}</h4>
               </div>
-              <p>{{ organization.text }}</p>
+              <!-- <p>{{ organization.text }}</p> -->
             </div>
           </div><!-- End testimonial item -->
         </div>
@@ -39,34 +39,46 @@ export default {
       organizations: [
         {
           index:"1",
-          text: "Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.",
+          // text: "Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.",
           image: "assets/img/testimonials/testimonials-1.jpg",
-          name: "Saul Goodman",
-          position: "Ceo & Founder"
+          name: "Alno Ramalino",
+          position: "Ceo"
         },
         {
-          text: "Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.",
+          // text: "Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.",
           image: "assets/img/testimonials/testimonials-2.jpg",
-          name: "Sara Wilsson",
-          position: "Designer"
+          name: "Anjas Nurheri",
+          position: "Managing Director"
         },
         {
-          text: "Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.",
+          // text: "Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.",
           image: "assets/img/testimonials/testimonials-3.jpg",
           name: "Jena Karlis",
           position: "Store Owner"
         },
         {
-          text: "Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.",
+          // text: "Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.",
           image: "assets/img/testimonials/testimonials-4.jpg",
-          name: "Matt Brandon",
-          position: "Freelancer"
+          name: "Mufrizal Piliang",
+          position: "COO (Chieft Operating Officer)"
         },
         {
-          text: "Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.",
+          // text: "Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.",
           image: "assets/img/testimonials/testimonials-5.jpg",
-          name: "John Larson",
-          position: "Entrepreneur"
+          name: "Defni",
+          position: "Managing IT Developer"
+        },
+        {
+          // text: "Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.",
+          image: "assets/img/testimonials/testimonials-5.jpg",
+          name: "Firman Ardiansyah",
+          position: "Staff IT Desk"
+        },
+        {
+          // text: "Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.",
+          image: "assets/img/testimonials/testimonials-5.jpg",
+          name: "Aisyah avini",
+          position: "Staff Media Sosial & Kreatif"
         }
       ]
     }

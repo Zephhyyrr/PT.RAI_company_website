40bce7d992a346263dd31d60975051414a0d8edc
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2024-11-11 23:19:26 +0700

    add vanilla product

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -6,7 +6,7 @@
         <div class="container text-center">
             <div class="d-flex flex-column justify-content-center align-items-center">
                 <h1 data-aos="fade-up">Welcome to <span>Rumpun Alam Indonesia</span></h1>
-                <p data-aos="fade-up" data-aos-delay="100">Perusahaan Exportir Rempah-Rempah dari Indonesia<br></p>
+                <p data-aos="fade-up" data-aos-delay="100">Spices Exporting Company from Indonesia<br></p>
                 <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
                 </div>
             </div>
